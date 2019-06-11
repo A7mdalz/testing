06fd9cc6d14f1a325ef4a2edd85ecdf85253ec78
Ahmad Alzughaibi <ahmad.alz.git@gmail.com>
2019-06-11 16:40:34 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var http = require('http');
 
 http.createServer(function (req, res) {
-    res.writeHead(2000, {'Content-Type': 'text/plain'});
+    res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('Hello from the container!!!!!!!!!!!');
 }).listen(80);

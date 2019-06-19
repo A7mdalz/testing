85aaff774881426aa83fc82775f14b0b8a248914
hareeqi <hareeqi@gmail.com>
2019-06-19 14:26:08 +0300

    first demo mohammad aldossary

--- app.js
+++ app.js
@@ -1,6 +1,63 @@
-var http = require('http');
+const fs = require("fs");
+const path = require("path");
+const url = require("url");
+const http = require("http");
+const mimeType = {
+  ".ico": "image/x-icon",
+  ".html": "text/html",
+  ".js": "text/javascript",
+  ".json": "application/json",
+  ".css": "text/css",
+  ".png": "image/png",
+  ".jpg": "image/jpeg",
+  ".wav": "audio/wav",
+  ".mp3": "audio/mpeg",
+  ".svg": "image/svg+xml",
+  ".pdf": "application/pdf",
+  ".doc": "application/msword",
+  ".eot": "appliaction/vnd.ms-fontobject",
+  ".ttf": "aplication/font-sfnt"
+};
+http
+  .createServer(function(req, res) {
+    console.log(`${req.method} ${req.url}`);
 
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('<h1 style="text-align: center;"><span style="color: #0000ff;"><strong>Hello from the container!!</strong></span></h1>');
-}).listen(80);
+    // parse URL
+    const parsedUrl = url.parse(req.url);
+
+    // extract URL path
+    // Avoid https://en.wikipedia.org/wiki/Directory_traversal_attack
+    // e.g curl --path-as-is http://localhost:9000/../fileInDanger.txt
+    // by limiting the path to current directory only
+    const sanitizePath = path.normalize(parsedUrl.pathname).replace(/^(\.\.[\/\\])+/, "");
+    let pathname = path.join(__dirname, sanitizePath);
+
+    fs.exists(pathname, function(exist) {
+      if (!exist) {
+        // if the file is not found, return 404
+        res.statusCode = 404;
+        res.end(`File ${pathname} not found!`);
+        return;
+      }
+
+      // if is a directory, then look for index.html
+      if (fs.statSync(pathname).isDirectory()) {
+        pathname += "/index.html";
+      }
+
+      // read file from file system
+      fs.readFile(pathname, function(err, data) {
+        if (err) {
+          res.statusCode = 500;
+          res.end(`Error getting the file: ${err}.`);
+        } else {
+          // based on the URL path, extract the file extention. e.g. .js, .doc, ...
+          const ext = path.parse(pathname).ext;
+          // if the file is found, set Content-type and send data
+          res.setHeader("Content-type", mimeType[ext] || "text/plain");
+          res.end(data);
+        }
+      });
+    });
+  })
+  .listen(80);

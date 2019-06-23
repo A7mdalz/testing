c9ff865b38e77a9b4a390ef63e5be1bf6dd490b8
hareeqi <hareeqi@gmail.com>
2019-06-23 16:01:20 +0300

    first real commit

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+const multer = require("multer");
+
+const upload = multer({ dest: "public/uploads/" });
+
 const port = 3000;
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+app.post("/uploadfile", upload.single("uploadinp"), (req, res, next) => {
+  const file = req.file;
+  res.sendStatus(200);
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));

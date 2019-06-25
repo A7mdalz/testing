5ed370b772d1ba7c93e724809ebc3af868c98c58
A7mdalz <ahmad.alz.git@gmail.com>
2019-06-25 10:55:52 +0300

    'await'

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const multer = require("multer");
-const fs = require('fs');
+const fs = require("fs");
 
 const upload_path = __dirname + "/public/uploads/";
 
-
+const sleep = millis => new Promise(resolve => setTimeout(resolve, millis));
 
 const storage = multer.diskStorage({
   destination: function(req, file, cb) {
@@ -24,14 +24,16 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.post("/uploadfile", upload.single("uploadinp"), (req, res, next) => {
+app.post("/uploadfile", upload.single("uploadinp"), async (req, res, next) => {
+  res.sendStatus(200);
+
   const file = req.file;
   console.log(file);
-  const src = upload_path+file.filename
-  const dst = file.filename
-  fs.copyFileSync(src,'/hrapp2/'+dst)
-  fs.copyFileSync(src,'/hrapp/'+dst)
-  res.sendStatus(200);
+  const src = upload_path + file.filename;
+  const dst = file.filename;
+  fs.copyFileSync(src, "/hrapp2/" + dst);
+  await sleep(2000);
+  fs.copyFileSync(src, "/hrapp/" + dst);
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
